b9508c4ca189428307a44316a24e8f488f150c48
gbangi <gbangi@uncc.edu>
2017-10-12 22:13:30 -0400

    added prefixsum code

--- prefixsum.cpp
+++ prefixsum.cpp
@@ -5,7 +5,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-
+#include <chrono>
 
 #ifdef __cplusplus
 extern "C" {
@@ -40,13 +40,53 @@ int main (int argc, char* argv[]) {
   }
 
   int * arr = new int [atoi(argv[1])];
-
+  int * pr = new int [atoi(argv[1])];
   generatePrefixSumData (arr, atoi(argv[1]));
   
   //write code here
+  int n = atoi(argv[1]);
+  int nbthreads = atoi(argv[2]);
 
+   // for(int k=0 ; k<n ; k++)
+   //     arr[k]=1;
 
+   std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
 
+  float *suma;
+    #pragma omp parallel num_threads(nbthreads)
+    {
+        const int ithread = omp_get_thread_num();
+        //const int nthreads = omp_get_num_threads();
+        #pragma omp single
+        {
+            suma = new float[nbthreads+1];
+            suma[0] = 0;
+        }
+        float sum = 0;
+        #pragma omp for schedule(static)
+        for (int i=0; i<n; i++) {
+            sum += arr[i];
+            pr[i] = sum;
+        }
+        suma[ithread+1] = sum;
+        #pragma omp barrier
+        float offset = 0;
+        for(int i=0; i<(ithread+1); i++) {
+            offset += suma[i];
+        }
+        #pragma omp for schedule(static)
+        for (int i=0; i<n; i++) {
+            pr[i] += offset;
+        }
+    }
+    delete[] suma;
+
+    std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::cerr<<elapsed_seconds.count()<<std::endl;
+
+  //for(int i=0; i<n; i++) printf("%d %f\n", pr[i], 0.5*i*(i+1));
+  
   checkPrefixSumResult(pr, atoi(argv[1]));
   
   delete[] arr;
