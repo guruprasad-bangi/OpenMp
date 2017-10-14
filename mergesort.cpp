395e0c4db36964ebb929ddefac9ed1e44fc36d7d
gbangi <gbangi@uncc.edu>
2017-10-14 00:24:03 -0400

    commiting merge sort code

--- mergesort.cpp
+++ mergesort.cpp
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <algorithm>
+#include <chrono>
 
 #ifdef __cplusplus
 extern "C" {
@@ -19,6 +20,8 @@ extern "C" {
 }
 #endif
 
+void merge(int *a, int l, int m, int r);
+int min2(int a1, int a2);
 
 int main (int argc, char* argv[]) {
 
@@ -34,8 +37,8 @@ int main (int argc, char* argv[]) {
     }
   }
   
-  if (argc < 3) {
-    std::cerr<<"Usage: "<<argv[0]<<" <n> <nbthreads>"<<std::endl;
+  if (argc < 5) {
+    std::cerr<<"Usage: "<<argv[0]<<" <n> <nbthreads> <scheduling> <granularity>"<<std::endl;
     return -1;
   }
 
@@ -45,11 +48,94 @@ int main (int argc, char* argv[]) {
   
   //write code here
 
+  long n = atoi(argv[1]);
+  int nbthreads = atoi(argv[2]);
+  std::string scheduling = argv[3];
+  int granularity = atoi(argv[4]);
+
+  omp_set_num_threads(nbthreads);
+
+  if(scheduling == "static")
+        omp_set_schedule(omp_sched_static,granularity);
+      else if(scheduling == "dynamic")
+        omp_set_schedule(omp_sched_dynamic,granularity);
+      else if(scheduling == "guided")
+        omp_set_schedule(omp_sched_guided,granularity);
+      else std::cerr << "Invalid scheduling options entered" << std::endl;
+
+  std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
+
+  for (int len=1; len<=n-1; len = 2*len)
+  {
+    #pragma omp parallel for schedule(runtime)
+    for (int ls=0; ls<n-1; ls += 2*len)
+    {
+      int mid = min2(ls+len-1, n-1);
+      int right = min2(ls+(2*len)-1, n-1);
+      merge(arr, ls, mid, right);
+    }
+  }
      
-  
+  std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end-start;
+  std::cerr<<elapsed_seconds.count()<<std::endl;
+
   checkMergeSortResult (arr, atoi(argv[1]));
   
   delete[] arr;
 
   return 0;
+}
+
+int min2(int a1, int a2)
+{
+  return a1 < a2 ? a1 : a2;
+}
+
+void merge(int *a, int l, int m, int r)
+{
+  int n1 = m - l + 1, n2 = r - m;
+
+  // Creating left and right arrays
+  int L[n1], R[n2];
+  for (int i = 0; i < n1; i++)
+  {
+    L[i] = a[l + i]; // from l to l+n1-1
+  }
+  for (int j = 0; j < n2; j++)
+  {
+    R[j] = a[m + 1+ j]; // from m+1 to m+n2
+  }
+
+  
+  int i = 0, j = 0, k = l;
+
+  // Merging 2 arrays of length n1 & n2
+  while (i < n1 && j < n2)
+  {
+    if (L[i] <= R[j])
+    {
+      a[k++] = L[i++];
+    }
+    else
+    {
+      a[k++] = R[j++];
+    }
+  }
+
+  if(j == n2) // in case R finishes first, leftover elements of L will get inserted as it is
+  {
+    while (i < n1)
+    {
+      a[k++] = L[i++];
+    }
+  }
+  else if(i == n1) // in case L finishes first, leftover elements of R will get inserted as it is
+  {
+    while (j < n2)
+    {
+      a[k++] = R[j++];
+    }
+  }
+  return;
 }
